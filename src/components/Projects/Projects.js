31a49e2d06d696700f142e26caa907bf9e875552
Gabriel Pelosi <1180017@isep.ipp.pt>
2021-09-27 16:53:19 +0100

    hedae, project and tech changes

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,48 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+const projectsDemo = [
+  {title: 'aa', description: 'aaaaa'},
+  {title: 'aa', description: 'aaaaa'},
+  {title: 'aa', description: 'aaaaa'},
+  {title: 'aa', description: 'aaaaa'},
+  {title: 'aa', description: 'aaaaa'},
+  {title: 'aa', description: 'aaaaa'},
+  {title: 'aa', description: 'aaaaa'},
+]
+
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects"> 
+    <SectionDivider/> 
+      <SectionTitle main> Projects</SectionTitle> 
+        <GridContainer> 
+          {projects.map((project) => (
+            <BlogCard key={project.id}>
+              <Img src={project.image}/>
+              <TitleContent>
+                <HeaderThree title> {project.title} </HeaderThree>
+                <Hr/>
+              </TitleContent>
+              <CardInfo>{project.description}</CardInfo>
+              <div>
+                <TitleContent>Stack</TitleContent>
+                <TagList>
+                  {
+                    project.tags.map((tag,i) => (
+                        <Tag key={i}> {tag} </Tag>
+                    ))
+                  }
+                </TagList>
+              </div>
+              <UtilityList>
+                <ExternalLinks href={project.visit}>Code</ExternalLinks>
+                <ExternalLinks href={project.source}>Source</ExternalLinks>
+              </UtilityList>
+            </BlogCard>
+          ))}
+        </GridContainer>
+      
+  </Section>
 );
 
 export default Projects;
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,72 @@
 import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import {DiJava, DiReact, DiDocker,DiGitBranch, DiScrum } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id="tech">
+    <SectionDivider/>
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've workend and developed with many technologies in web development world.
+      From Back-end to Front-end applications.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="5rem"/>
+        <ListContainer>
+          <ListTitle>Front-end</ListTitle>
+          <ListParagraph>
+            Already developed <br/>
+            React apps with JavaScript and TypeScript, 
+            Html5, css3, Sass and taiwind
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiJava size="5rem"/>
+        <ListContainer>
+          <ListTitle>Back-end</ListTitle>
+          <ListParagraph>
+            Already developed <br/>
+            Apis with Java, Maven, Gradle, Spring boot and its dependencies
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiDocker size="5rem"/>
+        <ListContainer>
+          <ListTitle>Docker</ListTitle>
+          <ListParagraph>
+            Docker for developer and test <br/> databases containers
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiGitBranch size="5rem"/>
+        <ListContainer>
+          <ListTitle>Git</ListTitle>
+          <ListParagraph>
+            Worked with github and bitbucket <br/> 
+            for personal and academic projects
+          </ListParagraph>
+          <ListParagraph>
+            Also used gitflow in one project
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiScrum size="5rem"/>
+        <ListContainer>
+          <ListTitle>Methodologies</ListTitle>
+          <ListParagraph>
+            In all my academic projects, i have used Scrum <br/> 
+            as Methodology
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      </List>
+  </Section>
 );
 
 export default Technologies;
